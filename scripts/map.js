74f99922238d5872e46cef27d017798798c58e7c
DKandler <davidkandler@gmail.com>
2019-07-11 20:47:20 -0400

    updated JS and map API

--- scripts/map.js
+++ scripts/map.js
@@ -1,13 +1,116 @@
-function initMap() {
-  var uluru = {lat: 33.753746, lng: -84.386330};
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 2, center: uluru});
-  // The marker, positioned at Uluru
- 
-  //test Marker 
- // var marker = new google.maps.Marker({position: uluru, map: map});
+$( document ).ready(function() {
+  console.log( "ready!" );
+});
+
+// firebase.initializeApp({
+//   apiKey: "AIzaSyAHQfTJLUMgBDlx94jzXev8YalLgMwXzgI",
+//   authDomain: "landmarks-959fd.firebaseapp.com",
+//   projectId: '"landmarks-959fd"'
+// });
+
+window.initMap = function() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 10.924638, lng: -29.706279},
+    zoom: 2.5,
+    styles: [
+      {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+      {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+      {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+      {
+        featureType: 'administrative.locality',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#d59563'}]
+      },
+      {
+        featureType: 'poi',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#d59563'}]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'geometry',
+        stylers: [{color: '#263c3f'}]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#6b9a76'}]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry',
+        stylers: [{color: '#38414e'}]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry.stroke',
+        stylers: [{color: '#212a37'}]
+      },
+      {
+        featureType: 'road',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#9ca5b3'}]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry',
+        stylers: [{color: '#746855'}]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [{color: '#1f2835'}]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#f3d19c'}]
+      },
+      {
+        featureType: 'transit',
+        elementType: 'geometry',
+        stylers: [{color: '#2f3948'}]
+      },
+      {
+        featureType: 'transit.station',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#d59563'}]
+      },
+      {
+        featureType: 'water',
+        elementType: 'geometry',
+        stylers: [{color: '#17263c'}]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#515c6d'}]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.stroke',
+        stylers: [{color: '#17263c'}]
+      }
+    ]
+  });
+
+ //Adding marker from fire base
+ //define userLatLng as user locations  
+  
+ var userLatLng = ({lat: , lng: })
+  var userLandmarkName = ('')
+
+  var marker = new google.maps.Marker({
+    position: userLatLng,
+    map: map,
+    title: userLandmarkName
+  });
+}
+//getting data from firebase
+
+// var db = firebase.firestore();
+
+
 
   //Need to map markers through firestore
   
-
-}
