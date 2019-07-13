c9ec924a9c79fc106147d3b88f91052263db79ed
Anthony <agray0232@gmail.com>
2019-07-13 02:54:28 -0400

    Saving data (#18)
    * Revert "Merge branch 'dev' into saving-data"
    
    This reverts commit b4a7f06bfa8ed606cb1c9e16773a50bd5a9b5ddc.
    
    * Revert "Revert "Merge branch 'dev' into saving-data""
    
    This reverts commit dac1237108e5657c50754ddf8a71d94c372b56f0.
    
    * Revert "Revert "Revert "Merge branch 'dev' into saving-data"""
    
    This reverts commit d54cdae13765a4f0a459e198d316cbf95efdf961.
    
    * changes to data storage branch
    
    includes signout and data storing
    
    * merge conflict resolution
    
    fixing conflict.
    
    * Returned landmark results get written to firebase
    
    * Fixed duplicate problem
    
    * Added marker array, plot landmarks, and map contains to map js
    
    * Grabbed snapshot of database and sent full json to map function
    
    * Data from firebase is sent to map js through a promise, parsed, and plotted
    
    * Data written to database by landmark name instead of number
    
    * Did some stuff

---
+++ scripts/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAHQfTJLUMgBDlx94jzXev8YalLgMwXzgI",
+    authDomain: "landmarks-959fd.web.app",
+    databaseURL: "https://landmarks-959fd.firebaseio.com",
+    projectId: "landmarks-959fd",
+    storageBucket: "landmarks-959fd.appspot.com",
+    messagingSenderId: "428028328211",
+    appId: "1:428028328211:web:a00b8a622bf73b0d"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
