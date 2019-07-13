ebecad8d8315f48f39749907f31c4d9985d59b3e
Joshua Cox <joshdcox50@gmail.com>
2019-07-13 12:40:34 -0400

    login on get started button

--- scripts/login.js
+++ scripts/login.js
@@ -1,26 +1,31 @@
 // var ui = new firebaseui.auth.AuthUI(firebase.auth());
-document.getElementById('LoginBtn').addEventListener('click', function (e) {
-    e.preventDefault()
-    var uiConfig = {
-        callbacks: {
-            signInSuccessWithAuthResult: function (authResult, redirectUrl) {
-                return true;
-            },
-            uiShown: function () {
+const loginButtons = document.getElementsByClassName('js-login-button');
 
-            }
+var uiConfig = {
+    callbacks: {
+        signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+            return true;
         },
-        // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-        // signInFlow: 'popup',
-        signInSuccessUrl: 'pages/addTrip.html',
-        signInOptions: [{
-            provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-            requireDisplayName: false
-        }],
+        uiShown: function() {
 
-    };
-    var ui = new firebaseui.auth.AuthUI(firebase.auth());
-    ui.start('#firebaseui-auth-container', uiConfig);
+        }
+    },
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    // signInFlow: 'popup',
+    signInSuccessUrl: 'pages/addTrip.html',
+    signInOptions: [{
+        provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        requireDisplayName: false
+    }],
+
+};
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+$.each(loginButtons, (index, loginButton) => {
+    loginButton.addEventListener('click', function(e) {
+        e.preventDefault()
+        ui.start('#firebaseui-auth-container', uiConfig);
 
 
+    })
 })
